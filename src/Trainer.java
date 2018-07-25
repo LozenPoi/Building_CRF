ff9ea861527e1e5c72130695f6bf6402ec1843b4
zl5sv <zl5sv.cs.virginia.edu>
2018-07-25 17:12:10 -0400

    update

--- src/Trainer.java
+++ src/Trainer.java
@@ -1,4 +1,28 @@
-package utils;
+import java.util.ArrayList;
+import java.util.Vector;
+
+/**
+ *  This is a class putting node and edge features into table factors. The feature definition is specified in the class
+ *  "FeatureGenerator".
+ */
 
 public class Trainer {
+
+    public static ArrayList<String4Learning> string4Learning(String[] feature_dict, String[] label_dict,
+                                                             String train_path){
+
+        // Each string is stored as an object specifying features as table factors.
+        ArrayList<String4Learning> str_list = new ArrayList<>();
+
+        ArrayList<Vector> list_feature_vec;
+        ArrayList<Integer> label_vec;
+
+        // For each training sample, construct a factor graph and a list of factors where the list of factors stores
+        // edge and node features.
+
+
+
+    }
+
+
 }
