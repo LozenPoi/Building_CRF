e250969430e9c5b63edb77de60eb65415d0fcb80
zl5sv <zl5sv.cs.virginia.edu>
2018-07-27 16:30:17 -0400

    update

--- src/FeatureGenerator.java
+++ src/FeatureGenerator.java
@@ -1,14 +1,33 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * This is a class defining the feature types, and processing data to create dictionaries and return feature vectors.
+ */
+
 public class FeatureGenerator {
 
-    public static int numIns;
-    public static int numFeature;
-    public static int numClass;
-    public static int[] label;
-    public static ArrayList<HashMap<Integer, int[]>> featureTable;
+    public HashMap<Integer,String> dict_token;   // a dictionary for characters (tokens) in a string
+    public HashMap<Integer,String> dict_label;   // a dictionary for labels
+    public HashMap<Integer,String> dict_feature; // a dictionary for feature types
 
+    // Initialize this object.
+    public FeatureGenerator(){
+        dict_token = new HashMap<>();
+        dict_label = new HashMap<>();
+        dict_feature = new HashMap<>();
+    }
+
+    // Read data through files.
+    public void readData(){}
+
+    // Get the node features for all node feature types.
+    public void getNodeFeature(){}
+
+    // Get the edge features for all edge feature types.
+    public void getEdgeFeature(){}
+
+    //
 
 
 }
