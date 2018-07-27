57e4968bc103d2dcc3a15f89cdaecca80e65a9d3
LozenPoi <zl5sv@virginia.edu>
2018-07-27 18:11:12 -0400

    update feature generator

--- src/FeatureGenerator.java
+++ src/FeatureGenerator.java
@@ -7,15 +7,20 @@ import java.util.HashMap;
 
 public class FeatureGenerator {
 
-    public HashMap<Integer,String> dict_token;   // a dictionary for characters (tokens) in a string
-    public HashMap<Integer,String> dict_label;   // a dictionary for labels
-    public HashMap<Integer,String> dict_feature; // a dictionary for feature types
+    public HashMap<Integer,String> dict_token;          // a dictionary for characters (tokens) in a string
+    public HashMap<Integer,String> dict_label;          // a dictionary for labels
+    public HashMap<Integer,String> dict_node_feature;   // a dictionary for node feature types
+    public HashMap<Integer,String> dict_edge_feature;   // a dictionary for node feature types
 
     // Initialize this object.
     public FeatureGenerator(){
         dict_token = new HashMap<>();
         dict_label = new HashMap<>();
-        dict_feature = new HashMap<>();
+        dict_node_feature = new HashMap<>();
+        dict_edge_feature = new HashMap<>();
+        // Assign an ID to each feature type.
+        dict_node_feature.put(1,"token");
+        dict_edge_feature.put(1,"B followed by I");
     }
 
     // Read data through files.
@@ -27,7 +32,29 @@ public class FeatureGenerator {
     // Get the edge features for all edge feature types.
     public void getEdgeFeature(){}
 
-    //
+    // Update the token dictionary.
+    public void update_token(){}
+
+    // Update the label dictionary.
+    public void update_label(){}
+
+
+    // This is a node feature which is the character itself.
+    private int currentToken(char token){
+        int index;
+        if(dict_token.containsValue(token)){
+            // get the index
+        }
+        else{
+            // update the dictionary and get the index
+        }
+        return index;
+    }
+
+    // This is an edge feature which indicates label "B" followed by label "I".
+    private boolean B_to_I(){
+        //
+    }
 
 
 }
